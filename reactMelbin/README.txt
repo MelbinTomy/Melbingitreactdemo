6387d9b9402ecc1807a7ac24f1bacc7d6d234da7
MelbinTomy <melbintomy40@gmail.com>
2024-09-28 17:32:33 -0400

    New component added to the project

---
+++ reactMelbin/README.txt
@@ -0,0 +1,1 @@
+This is a React Vite project named reactMelbin for Assign1.
---
+++ reactMelbin/src/NewComponent.jsx
@@ -0,0 +1,8 @@
+// src/NewComponent.jsx
+import React from 'react';
+
+const NewComponent = () => {
+    return <div>Hello from New Component!</div>;
+};
+
+export default NewComponent;
